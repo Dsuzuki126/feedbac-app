8d6b8d2562388c45a2e27e288f211def1ecc7426
David Suzuki <David@suzuki-aloha.com>
2023-04-11 15:35:04 -0700

    testing out conditionals

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ function App() {
         {id: 1, text : 'hello'},
         {id: 2, text : 'bye'}
     ]
-    let test;
+    const loading = false;
+    const showComments = true;
+
+    
     return (
     <div>
         <h1>Hello from the app component</h1>
         <div>
             <h3>hellos</h3>
+            {showComments ? 'yes' : 'no'}
             <ul>
             {comments.map( (comment, index) => 
                 <li key={index}>{comment.text} </li>
