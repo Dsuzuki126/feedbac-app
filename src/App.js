f93ad7ed73fd97ae519aae89743b20ca5ba8b88a
David Suzuki <david@suzuki-aloha.com>
2023-05-22 16:45:37 -0700

    reset project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 function App() {
-    let comments = [
-        {id: 1, text : 'hello'},
-        {id: 2, text : 'bye'}
-    ]
-    const loading = false;
-    const showComments = true;
-
     
     return (
-    <div>
-        <h1>Hello from the app component</h1>
-        <div>
-            <h3>hellos</h3>
-            {showComments ? 'yes' : 'no'}
-            <ul>
-            {comments.map( (comment, index) => 
-                <li key={index}>{comment.text} </li>
-            )}
-
-            </ul>
-        </div>
+    <div className='container'>
+        <h1>My App</h1>
         
     </div>
     );
